546cb8c47b875d091c002f6c13bf116293ffa564
Tom Goossens <tgoossen@f1r3s3.codam.nl>
2025-02-24 16:33:41 +0100

    push

---
+++ cpp_01/ex00/Zombie.hpp
@@ -0,0 +1,17 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+#include <string>
+#include <iostream>
+
+
+class Zombie {
+public:
+    void announce(void);
+	void randomChump(std::string name);
+	Zombie* newZombie(std::string name);
+    Zombie(std::string name);
+    ~Zombie();
+private:
+    std::string name;
+};
+#endif
